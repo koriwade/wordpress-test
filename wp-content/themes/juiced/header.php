b144538115e3cb91df00b2e7cce6f73d038d9f40
Kori Wade <wade@teamsubjectmatter.com>
2016-06-29 13:39:18 -0400

    initial commit

---
+++ wp-content/themes/juiced/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php bloginfo('name'); ?></title>
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<header class="site_header">
+		<h1><?php bloginfo('name'); ?></h1>
+		<h3><?php bloginfo('description'); ?></h3>
+</body>
