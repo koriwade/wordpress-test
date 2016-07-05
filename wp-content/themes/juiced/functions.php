2e2d459035677afd02e3ede3a28bd22e501ec654
Kori Wade <wade@teamsubjectmatter.com>
2016-07-05 10:26:36 -0400

    more

--- wp-content/themes/juiced/functions.php
+++ wp-content/themes/juiced/functions.php
@@ -5,3 +5,9 @@ function testSite_recources() {
 }
 
 add_action('wp_enqueue_scripts' , 'testSite_recources');
+
+//navigation menus
+register_nav_menus(array(
+	'primary' => __( 'Primary Menu'),
+	'footer' => __('Footer Menu'),
+));
--- wp-content/themes/juiced/header.php
+++ wp-content/themes/juiced/header.php
@@ -3,13 +3,21 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php bloginfo('name'); ?></title>
+	<title><?php bloginfo('name') ?></title>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+<!-- site header -->
 	<div class="container">
 		<header class="site_header">
 			<h1><?php bloginfo('name'); ?></h1>
 			<h3><?php bloginfo('description'); ?></h3>
+
+<!-- nav menu -->
+			<nav class="site-nav">
+				<?php wp_nav_menu(); ?>
+			</nav>
+
 		</header>
 </body>
